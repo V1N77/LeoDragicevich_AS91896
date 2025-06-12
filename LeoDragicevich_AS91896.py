deb00730937bd5b783507e823a172eb7662f9ebd
V1N77 <159728348+V1N77@users.noreply.github.com>
2025-06-12 12:30:36 +1200

    Updat Big W

--- LeoDragicevich_AS91896.py
+++ LeoDragicevich_AS91896.py
@@ -1,1 +1,23 @@
-print("Choose your companion to start your journey")
+import random
+
+print("Choose you Companion to start your journey")
+
+class Companion:
+    def __init__(self, name, element, health, speed, damagemultiplier, defense, moves):
+        self.name
+        self.element
+        self.health
+        self.speed
+        self.damage_multiplier
+        self.defense
+        self.moves
+
+def show_stats(self):
+    
+    print("{self.name},{self.element},Stats:").format
+
+fire_lizard = Companion("Fire Lizard", "fire", health=40, speed=25, damagemultiplier=1.1*, )
+
+water_turtle = 
+
+plant_frog = 
