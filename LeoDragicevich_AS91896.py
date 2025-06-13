994f19af63da87217caca345e90e000d17d43aa3
V1N77 <159728348+V1N77@users.noreply.github.com>
2025-06-13 13:19:01 +1200

    new change yes

--- LeoDragicevich_AS91896.py
+++ LeoDragicevich_AS91896.py
@@ -12,12 +12,27 @@ class Companion:
         self.defense
         self.moves
 
+class Moves:
+    def __init__(self, name, element, damage):
+        self.name   
+        self.element
+        self.damage
+
+tackle = Moves("Tackle", "Normal", damage= 20)
+
+ember = Moves("Ember", "Fire", damage= 30)
+
+water_gun = Moves("Water Gun", "Water", damage= 30)
+
+vine_whip = Moves("Vine Whip", "Plant", damage= 30)
+
+
 def show_stats(self):
     
     print("{self.name},{self.element},Stats:").format
 
-fire_lizard = Companion("Fire Lizard", "fire", health=40, speed=25, damagemultiplier=1.1*, )
+fire_lizard = Companion("Fire Lizard", "Fire", health=100, speed=30, damagemultiplier=1.1 ,defense= 40, moves= tackle, ember)
 
-water_turtle = 
+water_turtle = Companion("Water Turtle", "Water", health=110, speed=25, damagemultiplier=1.0 ,defense= 45, moves= tackle, water_gun)
 
-plant_frog = 
+plant_frog = Companion("Plant Frog", "Plant", health=120, speed=20, damagemultiplier=0.9 ,defense= 50, moves= tackle, vine_whip)
